618bbdd6cc30634f3fe778ef1eb063d0e6c4ad99
Haerul Yuda Aditiya <haerulyudaaditiya@gmail.com>
2025-04-28 11:26:44 +0700

    first commit

--- resources/views/footer.blade.php
+++ resources/views/footer.blade.php
@@ -1,5 +1,5 @@
 <footer>
     <div class="text-center p-3">
-        © 2024 Pelatihan PHP - Fundamental Laravel 11. All rights reserved.
+        © 2024 User Bank Management System. All rights reserved.
     </div>
 </footer>
